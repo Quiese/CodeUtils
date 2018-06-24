5b8d05753ad35e3533289e00b9e2f084505da64c
Quiese <quiese@solusis.com.br>
2018-06-24 01:34:34 -0300

    Adicionada classe para tratamento de imagens

---
+++ CodeUtils/Images.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace CodeUtils
+{
+    public static class Images
+    {
+
+        public static byte[] ConvertImageToByteArray(Image imageToConvert,
+            System.Drawing.Imaging.ImageFormat formatOfImage)
+        {
+            byte[] ret;
+
+            using (var ms = new MemoryStream())
+            {
+                imageToConvert.Save(ms, formatOfImage);
+                ret = ms.ToArray();
+            }
+
+            return ret;
+        }
+
+        public static Image ConvertByteArrayToImage(byte[] byteArrayIn)
+        {
+            var ms = new MemoryStream(byteArrayIn);
+            var returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+
+    }
+}
